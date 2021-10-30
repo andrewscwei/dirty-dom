d0af0b863190db9866ef31eb15c2a2021b7a9442
Andrew Wei <andrewscwei@gmail.com>
2021-10-30 10:08:44 -0700

    Add license info

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
+/** @license dirty-dom
+ * © Andrew Wei
+ * This source code is licensed under the MIT license found in the LICENSE file in the root
+ * directory of this source tree.
+ */
+
 export { default as ScrollDelegate } from './core/ScrollDelegate'
 export { default as StickyCrossScrollDelegate } from './core/StickyCrossScrollDelegate'
 export { default as StickyScrollDelegate } from './core/StickyScrollDelegate'

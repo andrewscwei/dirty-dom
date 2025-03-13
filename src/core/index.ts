6fa32353e6c8b54ee2ddd67bd56cfd4521d8f9f1
andrewscwei <andrewscwei@users.noreply.github.com>
2025-03-13 23:35:50 +0000

    [SKIP CI] fix: Explicitly define exports in package.json

--- src/core/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './ScrollDelegate.js'
-export * from './StickyCrossScrollDelegate.js'
-export * from './StickyScrollDelegate.js'
-export * from './UpdateDelegate.js'
--- src/utils/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './cancelAnimationFrame.js'
-export * from './debounce.js'
-export * from './requestAnimationFrame.js'
-export * from './scroll.js'
